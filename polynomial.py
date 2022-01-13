a3cfa561ad2a8e9cdc0ff993f1c56a9a78a60d21
Always Good Apps <macbookpro@theone.local>
2022-01-13 20:26:06 +0000

    Changes added

--- polynomial.py
+++ polynomial.py
@@ -5,17 +5,25 @@ import matplotlib.pyplot as plt  # type:ignore
 class Poly2:
     """ Classe permettant de representer un polynôme de degré 2."""
 
-    def __init__(self, *coeffs):
+    def __init__(self, a, b, c):
         """ Méthode constructeur qui prend en paramètre, les coefficients du polynôme"""
-        pass
+        self.a = a
+        self.b = b
+        self.c = c
 
     def __add__(self, other):
         """Addition 2 polynômes et qui renvoi du nouveau polynôme"""
-        pass
+        self.a = self.a + other.a
+        self.b = self.b + other.b
+        self.c = self.c + other.c
+        return self
 
     def __sub__(self, other):
         """Soustraction de 2 polynômes et renvoi du nouveau polynôme"""
-        pass
+        self.a = self.a - other.a
+        self.b = self.b - other.b
+        self.c = self.c - other.c
+        return self
 
     def __repr__(self):
         msg = 'Poly2(' + ', '.join([str(c) for c in sorted(self.coeffs.values())]) + ')'
@@ -26,11 +34,36 @@ class Poly2:
         Si: p1 = Poly(3, -4, 2)
         Alors print(p1) affiche: '2X^2 - 4X + 3'
         """
-        pass
+        return "{}X^2{:+}X{:+}".format(self.c,self.b,self.a)
 
     def solve(self):
         """ Méthode qui renvoie les solutions si elles existent."""
-        pass
+        a = self.c
+        b = self.b
+        c = self.a
+
+        dis = b * b - 4 * a * c 
+        sqrt_val = sqrt(dis)
+        
+        # checking condition for discriminant
+        if dis > 0: 
+            x1 = (-b - sqrt_val(dis)) /(2*a)
+            x2 = (-b + sqrt_val(dis)) /(2*a)
+            return x1, x2
+      
+        
+        elif dis == 0: 
+            return f"{-b / (2 * a)}"
+        
+        # when discriminant is less than 0
+        else:
+            print() 
+            print()
+            x1 = - b / (2 * a), "+", sqrt_val
+            x2 = - b / (2 * a), "-", sqrt_val
+            return x1, x2
+    
+
 
     def __val(self, x):
         """ Méthode qui calcule et renvoie la valeur de y en fonction de x.
@@ -42,16 +75,27 @@ class Poly2:
 
     def draw(self, x_points=None):
         """ Méthode qui trace la courbe, voir fichier png."""
-        pass
+        
+    
+        # La courbe
+        plt.plot(x)
 
+        # Les axes
+        plt.axvline(x=0, color ='r')
+        plt.axhline(y=0, color ='r')   
+        axes = plt.gca()
+        axes.set_xlabel('x : abscisse')
+        axes.set_ylabel('f(x) : ordonnée')
+
+        plt.show()
 
 if __name__ == "__main__":
     bar = [1, 1, 1]
     p1 = Poly2(*bar)
-
+    
     baz = [1, 1, 1]
     p2 = Poly2(*baz)
-
+    
     p3 = p1 + p2
     print(p3)  # affiche 2x^2 + 2x + 2
 
